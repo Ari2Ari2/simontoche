e3b5924a105c3bb7c3ac299db3c2e38f8b2296e6
tamerdark <tamerdark@gmail.com>
2012-02-26 22:21:43 +0000

    Creado paquete para manejar la logica del juego independiente del framework de despliege. Creado tambien la estructura basica de clases.

---
+++ Simon Toche/src/simonToche/logic/Game.java
@@ -0,0 +1,19 @@
+/** Mantiene los parametros del juego
+ * 
+ */
+
+package simonToche.logic;
+
+public class Game {
+	
+	private static double timeFactor;
+	private static byte foodLevel;
+	private static byte studyLevel;
+	private static byte sleepLevel;
+	private static byte funLevel;
+	private static byte week;
+	private static byte day;
+	private static byte hour;
+	private static Place place;
+
+}
